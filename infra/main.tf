5fbbb9c0fd7548583a61ab2c3c45f2608c3ef33c
K-Hindusha <220701092@rajalakshmi.edu.in>
2025-08-26 22:52:11 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,11 +1,27 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "peer-tutoring-rg"
-  location = "East Asia"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
 }
 
-resource "azurerm_static_site" "swa" {
+provider "azurerm" {
+  features {}
+}
+
+# Static Web App (new resource type)
+resource "azurerm_static_web_app" "swa" {
   name                = "peer-tutoring-frontend"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = "peer-tutoring-rg"   # use the RG you already created
   location            = "East Asia"
-  sku_tier            = "Free"
+
+  sku_tier = "Free"
+  sku_size = "Free"
+}
+
+# Output: URL of static web app
+output "static_web_app_url" {
+  value = azurerm_static_web_app.swa.default_host_name
 }
