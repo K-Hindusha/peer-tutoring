c2c745a4ea9001474a1294d847416f8cce2bd763
K-Hindusha <220701092@rajalakshmi.edu.in>
2025-08-27 22:35:46 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,22 @@
-# Resource group
-data "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+# main.tf
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "peer-tutoring-rg"
   location = var.location
 }
 
-# Static Web App (Free tier)
+# Create Static Web App inside that RG
 resource "azurerm_static_web_app" "swa" {
-  name                = var.static_app_name
+  name                = "peer-tutoring-swa"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  sku_tier            = "Free"
 
-  sku_tier = "Free"
-  sku_size = "Free"
+  tags = {
+    project = "peer-tutoring"
+  }
 }
+
+
 
