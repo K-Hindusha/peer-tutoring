b42e544651bbe9c0f93b451f88ff82c1555af252
K-Hindusha <220701092@rajalakshmi.edu.in>
2025-08-27 20:19:27 +0530

    Add files via upload

---
+++ infra/main.tf
@@ -0,0 +1,16 @@
+# Resource Group
+resource "azurerm_resource_group" "peer_rg" {
+  name     = "peer-tutoring-rg"
+  location = var.location
+}
+
+# Static Web App
+resource "azurerm_static_web_app" "peer_swa" {
+  name                = "peertutoringapp"   # must be unique, lowercase, no spaces
+  resource_group_name = azurerm_resource_group.peer_rg.name
+  location            = var.location
+
+  sku_tier = "Free"
+  sku_size = "Free"
+}
+
