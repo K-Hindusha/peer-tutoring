4f8369c379d41833a36272cbaa21188319e89a2a
K-Hindusha <220701092@rajalakshmi.edu.in>
2025-08-27 20:15:25 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,5 @@
-output "webapp_url" {
-  value = azurerm_linux_web_app.peer_app.default_hostname
+output "static_web_app_url" {
+  description = "Default URL of the Static Web App"
+  value       = azurerm_static_web_app.peer_swa.default_host_name
 }
+
